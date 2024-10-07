536748756f179660fe6bd1136d4c7b18188df541
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2024-10-07 15:01:00 -0700

    Added new implentations to review

--- src/review.java
+++ src/review.java
@@ -1,9 +1,41 @@
 /**
  * review
+ * 
  */
-public class review {
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class review {
     public static void main(String[] args) {
-        System.out.println(" Hello, World");
+
+        //store the flavors and initialize
+        HashMap<String, String> flavorDescriptions = new HashMap<>();
+        flavorDescriptions.put("Vanilla", ", classic and sweet!");
+        flavorDescriptions.put("Chocolate", ", Rich and creamy!");
+        flavorDescriptions.put("Strawberry", ", Sweet and fruity!");
+        flavorDescriptions.put("Mango", ", Tropical and refreshing!");
+        flavorDescriptions.put("Raspberry", ", Tangy and sweet!");
+
+        //array of flowers for display
+        String[] flavors ={"Vanilla", "Chocolate", "Strawberry", "Mango", "Raspberry"};
+        System.out.print("Here's our flavors selection: ");
+
+        for (String flavor : flavors) {
+            System.out.println(flavor);
+        }
+
+        Scanner flavorInput = new Scanner(System.in); // Corrected constructor
+        System.out.print("Enter your flavor: ");
+        String userFlavor = flavorInput.nextLine(); // Read user input
+
+        if (flavorDescriptions.containsKey(userFlavor)) {
+            System.out.print("Here you go! " + userFlavor);
+            System.out.println(flavorDescriptions.get(userFlavor));
+        } else {
+            System.out.println("Seems we don't have that flavor :(");
+        }
+        
+        flavorInput.close();
     }
 }
